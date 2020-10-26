7ce8e6dd08da2d8fdbdbce040b898fd234dd4ad0
vanshita14 <54315644+vanshita14@users.noreply.github.com>
2020-10-26 13:47:12 +0530

    Create BoundaryFill.CPP

---
+++ BoundaryFill.CPP
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <conio.h>
+#include <graphics.h>
+
+void boundfill(int xc, int yc, int r, int b) {
+    int cur;
+    cur = getpixel(xc, yc);
+    if (cur != b && cur != r) {
+        putpixel(xc, yc, r);
+        delay(1);
+
+        boundfill(xc + 1, yc, r, b);
+        boundfill(xc - 1, yc, r, b);
+        boundfill(xc, yc + 1, r, b);
+        boundfill(xc, yc - 1, r, b);
+    }
+}
+
+void main() {
+    int gd = DETECT, gm;
+    initgraph(&gd, &gm, "..\\bgi");
+
+   rectangle(50, 50, 100 ,100);
+   boundfill(55, 55, 4, WHITE);
+    getch();
+    closegraph();
+}
